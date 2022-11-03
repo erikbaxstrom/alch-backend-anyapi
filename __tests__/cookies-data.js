0606b8565d308fe60538066f63143941a839f40f
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-03 12:50:45 -0700

    fix test

--- __tests__/cookies-data.js
+++
@@ -1,9 +0,0 @@
-const cookies = [
-  { id: 1, name: 'Sugar Cookie' },
-  { id: 2, name: 'Macaron' },
-  { id: 3, name: 'Gingerbread Humanoid' },
-  { id: 4, name: 'Date Swirl' },
-  { id: 5, name: 'Chocolate Crinkle' },
-];
-
-module.exports = { cookies };
