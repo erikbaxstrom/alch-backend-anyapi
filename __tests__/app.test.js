b5db2d97abccc70632c53ef332f1a8d770e596a1
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-03 10:46:41 -0700

    add expected data for cookies route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,8 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+const { cookies } = require('./cookies-data.js');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
---
+++ __tests__/cookies-data.js
@@ -0,0 +1,9 @@
+const cookies = [
+  { id: 1, name: 'Sugar Cookie' },
+  { id: 2, name: 'Macaron' },
+  { id: 3, name: 'Gingerbread Humanoid' },
+  { id: 4, name: 'Date Swirl' },
+  { id: 5, name: 'Chocolate Crinkle' },
+];
+
+module.exports = { cookies };
