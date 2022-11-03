85de4e494fd1c82c673abb6b2d8b7942c68963db
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-03 11:49:22 -0700

    implement test for cookies route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,14 +4,30 @@ const request = require('supertest');
 const app = require('../lib/app');
 const { cookies } = require('./cookies-data.js');
 
-describe('backend-express-template routes', () => {
+describe('cookies routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/cookies should return a list of cookies and their ids', async () => {
+    const res = await request(app).get('/cookies');
+    const expected = cookies.map((cookie) => {
+      return { id: cookie.id, name: cookie.name };
+    });
+    expect(res.body).toEqual(expected);
   });
   afterAll(() => {
     pool.end();
   });
 });
+
+// describe('backend-express-template routes', () => {
+//   beforeEach(() => {
+//     return setup(pool);
+//   });
+//   it('example test - delete me!', () => {
+//     expect(1).toEqual(1);
+//   });
+//   afterAll(() => {
+//     pool.end();
+//   });
+// });
